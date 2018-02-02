26b2422c4153fedf4a9c93e201d1fe39ebe1f1c5
grieco <grieco@freemail.hu>
2018-02-02 02:41:30 +0100

    Initial helloworld commit

---
+++ src/main/java/com/grieco/Starter.java
@@ -0,0 +1,22 @@
+package com.grieco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Starter
+{
+    @RequestMapping("/")
+    public String home()
+    {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(Starter.class, args);
+    }
+}
